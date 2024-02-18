a542d39f20052b07fd54225a3b9a312db3eb863f
fasil712 <fasilgetie12@gmail.com>
2024-02-18 21:23:38 +0300

    cookie parsing

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import routes from "./routes/index.mjs";
 
 const app = express();
 app.use(express.json());
 
+app.use(cookieParser());
+app.use(routes);
+
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
+  res.cookie("Hello", "World", { maxAge: 5000 * 10 });
   res.send({ res: "Its Working !!!" });
 });
-
-app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Running with port ${PORT}.`);
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,6 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
   res.send({ name: "IPhone", price: 2300, quantity: 2 });
 });
 
