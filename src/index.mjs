57098cb579abe4b6b1f5abcf856292aaedcc4105
fasil712 <fasilgetie12@gmail.com>
2024-02-18 21:56:26 +0300

    session section

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,21 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 import routes from "./routes/index.mjs";
 
 const app = express();
 app.use(express.json());
-
 app.use(cookieParser("cookie_secret"));
+app.use(
+  session({
+    secret: "fasil session",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
 app.use(routes);
 
 const PORT = process.env.PORT || 4000;
