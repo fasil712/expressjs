497eea776c3a1c97ab36a4ce0401f2620c3fd507
fasil712 <fasilgetie12@gmail.com>
2024-02-20 21:30:30 +0300

    start auth

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import routes from "./routes/index.mjs";
+import { monckUsers } from "./utils/constants.mjs";
 
 const app = express();
 app.use(express.json());
@@ -23,8 +24,21 @@ const PORT = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   console.log(req.session);
   console.log(req.session.id);
-  res.cookie("hello", "World", { maxAge: 5000 * 10, signed: true });
+  req.session.visited = true;
+  res.cookie("hello", "World", { maxAge: 50000 * 10, signed: true });
   res.send({ res: "Its Working !!!" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { email, password },
+  } = req;
+  const findUser = monckUsers.find((user) => user.email === email);
+  if (!findUser || findUser.password !== password) {
+    return res.status(401).send({ msg: "BAD CREADENTIALS" });
+  }
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
 });
 
 app.listen(PORT, () => {
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,4 +1,9 @@
 export const monckUsers = [
-  { id: 1, name: "Fasil Getie", email: "fasil@gmail.com" },
-  { id: 2, name: "Hana Getie", email: "hana@gmail.com" },
+  {
+    id: 1,
+    fullname: "Fasil Getie",
+    password: "abc123",
+    email: "fasilg@gmail.com",
+  },
+  { id: 2, name: "Hana Getie", password: "321cba", email: "hanag@gmail.com" },
 ];
